5839eec94886d4cb7651336119d6fe5c0f4ec1c8
Wolf Logan <wolf@circle-cross.org>
2020-05-23 17:26:15 -0700

    Initial commit as An Tir Heralds website

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './black-lion-seal-red-trumpets.png';
 import './App.css';
 
 function App() {
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          An Tir College of Heralds
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
